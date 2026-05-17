0d0f0f35bf4b6cdf41c4eb148703b3ae3d051dab
ffacusuarez <suarezfacundo460@gmail.com>
2026-05-17 18:14:29 -0300

    Desafio 2 terminado

--- desafio2/main.py
+++ desafio2/main.py
@@ -4,32 +4,39 @@ def procesar_penales(secuencia):
     Analiza la frecuencia de tiros y determina la dirección dominante.
     Aplica la prioridad táctica L > R > C en caso de empate.
     
-    Args:
-        secuencia (str): Cadena de caracteres con direcciones L, R o C.
+    Parametro que recibe:
+        secuencia (str): Cadena de caracteres con direcciones L, R o C. Si no es alguna de esas letras, la ignora.
         
-    Returns:
-        tuple: (dirección_frecuente, cantidad) o (None, None) si es inválida.
+    Retorna:
+        tupla: (dirección_frecuente, cantidad) o (None, None) si es inválida.
     """
 
 
     if 1 <= len(secuencia) <= 1000:
 
-        contadores = {'L': 0, 'R': 0, 'C': 0}
+        contadores = {
+            'L': 0, 
+            'R': 0, 
+            'C': 0
+        }
 
         for tiro in secuencia:
             if tiro.upper() in contadores:
                 contadores[tiro.upper()] += 1
+
+        if contadores['L'] == 0 and contadores['R'] == 0 and contadores['C'] == 0:
+            return None, None
                             
-            max_dir = 'L'
-            max_cant = contadores['L']
+        max_dir = 'L'
+        max_cant = contadores['L']
                 
-            if contadores['R'] > max_cant:
-                max_dir = 'R'
-                max_cant = contadores['R']
+        if contadores['R'] > max_cant:
+            max_dir = 'R'
+            max_cant = contadores['R']
                     
-            if contadores['C'] > max_cant:
-                max_dir = 'C'
-                max_cant = contadores['C']
+        if contadores['C'] > max_cant:
+            max_dir = 'C'
+            max_cant = contadores['C']
 
         return max_dir, max_cant
 
@@ -38,6 +45,16 @@ def procesar_penales(secuencia):
 
 
 def main():
+    """
+    Función principal del programa.
+    
+    Se encarga de la entrada/salida de datos: lee la secuencia de penales 
+    desde el archivo de texto provisto (penales.txt), invoca la función de procesamiento 
+    y muestra los resultados.
+
+    """
+
+    
     try:
         with open("desafio2/penales.txt", "r") as archivo:
 
